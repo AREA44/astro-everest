85d2102bab6a1a3da912b0f7a3c5e3a62e58bb17
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2023-06-26 23:26:11 +0700

    🚀deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://area44.github.io',
+  base: '/astro-spaceships',
   integrations: [alpinejs(), tailwind()],
 });
