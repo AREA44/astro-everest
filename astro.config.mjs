29ba175267160dd3ca8442dc5871865330a1baa1
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2025-01-25 00:17:31 +0700

    TailwindCSS v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import alpinejs from '@astrojs/alpinejs'
-import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI ? 'https://area44.github.io' : 'http://localhost:3000',
   base: process.env.CI ? '/astro-everest/' : undefined,
-  integrations: [alpinejs(), tailwind()],
+  integrations: [alpinejs()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
